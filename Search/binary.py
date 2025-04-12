5431eb6b6d325e60b1aa9dffc3e05960808cf04f
Ostap Khomiak <hamster.ostap@gmail.com>
2025-04-12 17:10:07 +0200

    Small tweaks to Binary Search

--- Search/binary.py
+++ Search/binary.py
@@ -4,15 +4,16 @@ def binary_search(nums, target):
     # Time complexity: O(log n)
     # Space complexity: O(1)
     left, right = 0, len(nums) - 1
-    while left <= right:
+    while left < right:
         mid = left + (right - left) // 2
         if nums[mid] == target:
             return mid
         elif nums[mid] < target:
             left = mid + 1
         else:
-            right = mid - 1
+            right = mid
 
+    return -1
 
 
 nums = [3, 1, 2, 4, 6, 5]
